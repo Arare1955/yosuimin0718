751e628712300e6839bfdfb7fb86f09e9f75dee7
ythknakamura <ythknakamura@users.noreply.github.com>
2024-07-21 14:52:14 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# yosuimin0718
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/ythknakamura/yosuimin0718)
---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<div class="bg-gradient-to-br from-yellow-100 to-orange-300 h-svh w-svw">
+  <div class="h-svh max-w-xl flex flex-col gap-4 justify-center">
+    <div class="bg-orange-200">レベル表示</div>
+    <div class="bg-blue-200">スコア表示</div>
+    <div class="bg-green-200 aspect-square">盤面</div>
+    <div class="bg-yellow-200">残り時間</div>
+    <div class="bg-pink-200">ヒントの表示スペース</div>
+  </div>
+</div>
